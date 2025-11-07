2aa872bfd9ede987118ea2e16b9085c9aa1e1eb7
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2025-11-07 13:15:12 +0200

    added additional ref test

--- test/expect.spec.ts
+++ test/expect.spec.ts
@@ -121,7 +121,8 @@ describe('expect matchers', () => {
     // toDeepEqual
     test('toDeepEqual sorted', () => {
         vitestExpect(() => expect([1, 2]).toDeepEqual([2, 1])).not.toThrow();
-        vitestExpect(() => expect([1, 2, 3]).toDeepEqual([1, 2])).toThrow();
+        vitestExpect(() => expect([[1, 2], [1, 2, 3]]).toDeepEqual([[1, 2], [1, 2, 3]])).not.toThrow();
+        vitestExpect(() => expect([{a: 42}, {b: 'text'}]).toDeepEqual([{a: 42}, {b: 'text'}])).not.toThrow();
     });
 
     // toHaveLength
